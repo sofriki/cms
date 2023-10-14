91d431d22c0578aab89fffe7f7d0e64356bf2e3d
Aiken Tine Ahac <ahac.aiken@gmail.com>
2023-10-15 00:29:00 +0200

    ??

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST'),
-  url: env('PUBLIC_URL'),
+  url: env('PUBLIC_URL') + '/api',
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
